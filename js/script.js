dc73278d3a017c2fcfab9e42122c97d96b4b5f47
astrel- <indigoastrel@gmail.com>
2014-11-09 00:09:06 +0300

    pizda with ractive

--- js/script.js
+++ js/script.js
@@ -1,7 +1,19 @@
+function load() {
+	var ractive = new Ractive({
+						  el: 'main',
+						  template: '#template',
+						  data: { title: 'Poka', text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum." }
+						  });
+}
+
 function myFunction(s) {
-	var el = document.getElementsByClassName("main");
-	for (var i = el.length - 1; i >= 0; i--) {
-		el[i].hidden = true;
-	};
-	el[s].hidden = false;
+	var ractive = new Ractive({
+							  el: 'main',
+							  template: '#template',
+							  data: { title: 'Privet', text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum." }
+							  });
+}
+
+function myFunction1(s) {
+	ractive.set('title', 'Poka');
 }

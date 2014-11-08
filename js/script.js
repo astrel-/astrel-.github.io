8d4fc33226a50bc09d5c80815a566b601347bcfb
astrel- <indigoastrel@gmail.com>
2014-11-08 23:24:05 +0300

    some js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
-function myFunction() {
-    document.getElementById("demo").innerHTML = "МАТЬ ЕБАЛ";
+function myFunction(s) {
+	var el = document.getElementsByClassName("main");
+	for (var i = el.length - 1; i >= 0; i--) {
+		el[i].hidden = true;
+	};
+	el[s].hidden = false;
 }

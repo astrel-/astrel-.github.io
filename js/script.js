9cc3bd79cc1017e4711c02a5ca21ef7d11641d6f
astrel- <indigoastrel@gmail.com>
2014-11-09 00:15:57 +0300

    ractive unleashed

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
-function load() {
-	var ractive = new Ractive({
-						  el: 'main',
-						  template: '#template',
-						  data: { title: 'Poka', text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum." }
+var ractive = new Ractive({
+						  counter: 0
+						  }
 						  });
-}
+
+document.getElementById( 'template' ).addEventListener( 'click', function () {
+													ractive.set( 'counter', ractive.get( 'counter' ) + 1 );
+													});
 
 function myFunction(s) {
 	var ractive = new Ractive({

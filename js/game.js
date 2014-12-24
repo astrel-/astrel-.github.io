b8bf463800ccd659f9167649f8e2a498685e1a1e
astrel- <indigoastrel@gmail.com>
2014-12-25 02:08:36 +0300

    director.js

--- js/game.js
+++ js/game.js
@@ -2,6 +2,8 @@ var itemsPerPage = 10;
 var game;
 
 function choose_game ( e, game_number ) {
+	$('.game_button').removeClass('active_game_button');
+	$(e).addClass('active_game_button');
 	var gameTemplate;
 	$.ajax( '/templates/gameTemplate.html' ).done( function (template)  {
 		gameTemplate = template;
--- js/router.js
+++ js/router.js
@@ -1,1 +1,30 @@
-/* */
+var steam = function () { console.log("steam");};
+var dota = function () { console.log("dota");};
+var cs_go = function () { console.log("cs-go");};
+var tf = function () {console.log("tf");};
+
+var routes = {
+	'/steam': steam,
+	'/dota': dota,
+	'/cs-go': cs_go,
+	'/tf': tf
+};
+
+
+var author = function () { console.log("author"); };
+var books = function () { console.log("books"); };
+var viewBook = function (bookId) {
+console.log("viewBook: bookId is populated: " + bookId);
+};
+
+var routes1 = {
+'/author': author,
+'/books': [books, function() {
+  console.log("An inline route handler.");
+}],
+'/books/view/:bookId': viewBook
+};
+
+var router = Router(routes);
+
+router.init();

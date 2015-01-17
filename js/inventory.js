7ad2008e7738ae98ade72c7c28ca4fdbe1078e9b
Yury <indigoastrel@gmail.com>
2015-01-17 23:40:04 +0300

    inventory.js needs to be fixed still

--- js/inventory.js
+++ js/inventory.js
@@ -1,12 +1,132 @@
 var numberOfElementsPerPage = 20;
-var currentPage = 1;
 var inventoryTemplate;
-var inventory;
-var descriptions;
-var keys;
+var inventoryRactive;
 
 
-showInventory();
+
+showInventory2();
+
+function showInventory2() {
+    $.when( fetchDataInventory(), $.ajax( "templates/inventoryTemplate.html" ))
+     .then(function( data, template ) {
+        console.log( data );
+        //console.log( template[0] );
+        inventoryRactive = makeRactive( "main", template[0], data);
+        //console.log( inventoryRactive );
+     });
+}
+
+
+function fetchDataInventory() {
+    var data = { 
+        currentPage: 1,
+        keys: new Array( numberOfElementsPerPage ),
+        id: new Array( numberOfElementsPerPage ),
+        images: new Array( numberOfElementsPerPage ),
+        currentItem: {
+            imageURL: '',
+            appid: '',
+            name: '',
+            marketable: 1,
+            tradable: 1,
+            type: '',
+            description: '',
+            tags: {
+                quality: '',
+                rarity: '',
+                type: '',
+                slot: '',
+                hero: ''
+            }
+        }
+    };
+
+    $.ajax({
+        dataType: "json",
+        url: 'json/dota2.json',
+        success: function ( dataJSON ) { 
+            parse1 ( dataJSON, data, true ); 
+            //showItem1(0);
+            console.log( dataJSON );
+        }
+    });
+    return data;
+}
+
+function parse1( dataJSON, data, onload) {
+    onload = onload || false;
+    var descriptions = dataJSON.rgDescriptions;
+
+    data.descriptions = descriptions;
+    var keys = Object.keys(descriptions);
+    data.keys = keys;
+    console.log(keys);
+    var currentPage = data.currentPage;
+    for (var itemNumber = numberOfElementsPerPage*(currentPage-1); itemNumber <numberOfElementsPerPage*currentPage; itemNumber++) {
+        currentItemNumber = itemNumber-numberOfElementsPerPage*(currentPage-1);
+        item = descriptions[keys[itemNumber]];
+        try {
+            data.id[currentItemNumber] =  item.market_name;
+            data.images[currentItemNumber] = item.icon_url;
+            //console.log(item.market_name);
+        }
+        catch (e) {
+            data.id[currentItemNumber] =  null;
+            data.images[currentItemNumber] = null;
+            //console.log("finished");
+        }
+    }
+    if ( onload ) {
+        var activeItem = descriptions[keys[0]];
+        parseInfoForItem( activeItem, data );
+    }
+}
+
+function parseInfoForItem( activeItem, data ) {
+    // FUCKING FIX THIS
+    dota = {currentItem: {
+            imageURL: '',
+            appid: '',
+            name: '',
+            marketable: 1,
+            tradable: 1,
+            type: '',
+            description: '',
+            tags: {
+                quality: '',
+                rarity: '',
+                type: '',
+                slot: '',
+                hero: ''
+            }
+        }};
+    data = data || dota;
+    data.currentItem.imageURL = activeItem.icon_url;
+    data.currentItem.name = activeItem.name;
+    data.currentItem.type = activeItem.type;
+    data.currentItem.description = activeItem.descriptions[0].value;
+    data.currentItem.tags.quality = activeItem.tags[0].name;
+    data.currentItem.tags.rarity = activeItem.tags[1].name;
+    data.currentItem.tags.type = activeItem.tags[2].name;
+    data.currentItem.tags.slot = activeItem.tags[3].name;
+    data.currentItem.tags.hero = activeItem.tags[4].name;
+    data.currentItem.tradable =  activeItem.tradable;
+    return data.currentItem;
+}
+
+function showItem1( e ) {
+    $.ajax({
+        dataType: "json",
+        url: 'json/dota2.json',
+        success: function ( dataJSON ) { 
+            var descriptions = dataJSON.rgDescriptions;
+            activeItem = descriptions[ e.id ];
+            console.log( activeItem );
+            currentItem = parseInfoForItem( activeItem );
+            inventoryRactive.set( "currentItem", currentItem );
+        }
+    });
+}
 
 function showInventory () {
     //var doc;
@@ -105,21 +225,6 @@ function parse ( data ) {
 
 }
 
-function showItem ( i ) {
-    var activeItem = descriptions[keys[i]];
-    inventory.set('activeItemURL', activeItem.icon_url);
-    inventory.set('name', activeItem.name);
-    inventory.set('appid', activeItem.appid);
-    inventory.set('type', activeItem.type);
-    inventory.set('description', activeItem.descriptions[0].value);
-    inventory.set('tags.quality', activeItem.tags[0].name);
-    inventory.set('tags.rarity', activeItem.tags[1].name);
-    inventory.set('tags.type', activeItem.tags[2].name);
-    inventory.set('tags.slot', activeItem.tags[3].name);
-    inventory.set('tags.hero', activeItem.tags[4].name);
-    inventory.set('tradable', activeItem.tradable);
-}
-
 $("document").ready(function($){
     $( "#previous-page" ).click( function () {
         console.log("success");
@@ -128,7 +233,9 @@ $("document").ready(function($){
 })
 
 function show_next(){
-    console.log("success");
-    showItem ( 1 );
+    console.log(this);
+    var index = $( ".idlol" ).index( this );
+    console.log( index );
+    showItem ( index );
 };
 
--- js/script.js
+++ js/script.js
@@ -1,131 +1,73 @@
-var main;
-var mainTemplate;
 var loginRactive;
-var loginTemplate;
-
-//show_main();
-login();
 
 
-$("document").ready(function($){
+$(function() {
     var nav = $('#login');
     var container = $('#container');
 
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 130) {
-            nav.addClass("f-nav");
-            container.css({'padding-top': '27px'}); //25px + 2px for borders
-        } else {
-            nav.removeClass("f-nav");
-            container.css({'padding-top': '0px'});
-        }
-    });
+    $(window)
+    	.scroll(function () {
+	        if ($(this).scrollTop() > 130) {
+	            nav.addClass("f-nav");
+	            container.css({'padding-top': '27px'}); //25px + 2px for borders
+	        } else {
+	            nav.removeClass("f-nav");
+	            container.css({'padding-top': '0px'});
+	        }
+	    });
 
-    $( ".game-button" ).click(function() {
-		var index = $( ".game-button" ).index( this ) + 1;
-		console.log( index );
-		choose_game( this, index );
-	});
+    $( ".game-button" )
+    	.click(function() {
+			var index = $( ".game-button" ).index( this ) + 1;
+			console.log( index );
+			choose_game( this, index );
+		});
 
     $( "#profile" )
     	.click(function() {
 			showProfile();
 		});
 
+	$.when( fetchData(), $.get( "templates/loginTemplate.html" )  )
+	 .then(function( data, template ) {
+		loginRactive = makeRactive( "login",
+									template[0],
+									data);
+	});
+
 });
+	
+function fetchData() {
+	var data = { 
+		signedIn: false,
+		user: {
+			pic: 'av.jpg',
+		 	name: 'Bubalus',
+		 	money: 900
+		}
+	};
+	return data;
+}
 
-
-function login () {
-	$.ajax( 'templates/login.html' )
-		.done( function (template)  {
-			loginTemplate = template;
-			loginRactive = new Ractive({
-				el: 'login',
-				template: loginTemplate,
-				data : {
-					signedIn: false,
-					user : {
-						pic: 'av.jpg',
-						name: 'Bubalus',
-						money: 900
-					}
-				}
-			});
-		})
-		.fail( function () {
-			console.log( "error" );
-		});		
+function makeRactive( el, template, data ) {
+	ractive = new Ractive({
+		el: el,
+		template: template,
+		data: data
+	});
+	return ractive;
 }
 
 
 //For debugging 
 function radioClicked ( a ) {
-	if ( a.value == 'y' ) 
+	if ( a.value == 'y' ) {
 		loginRactive.set( 'signedIn', true );
-	else 
+		$( "#profile" ).show(); 
+	}
+	else {
 		loginRactive.set( 'signedIn', false);
+		$( "#profile" ).hide();
+	}
 };
-
-function showProfile() {
-	console.log( "Showing Profile" );
-}
-
-function withdraw () {
-	$.ajax( 'templates/withdrawTemplate.html' ).then( function (template)  {
-		var withdrawTemplate = template;
-		main = new Ractive({
-						  el: 'main',
-						  template: withdrawTemplate,
-						  data : {
-						  	}
-	    });
-	});
-}
-
-function deposit () {
-	$.ajax( 'templates/depositTemplate.html' ).then( function (template)  {
-		var depositTemplate = template;
-		main = new Ractive({
-						  el: 'main',
-						  template: depositTemplate,
-						  data : {
-						  	}
-	    });
-	});
-}
-
-function logout () {
-	right_sidebar.set( 'signedIn', true);
-	show_main();
-};
-
-function show_main () {
-	$.ajax( 'templates/mainTemplate.html' ).then( function (template)  {
-		mainTemplate = template;
-		main = new Ractive({
-							  el: 'main',
-							  template: mainTemplate,
-							  data : {
-							  }
-							  });
-});
-}
-
-
-$.ajax( 'templates/rightSidebarTemplate.html' ).then( function (template)  {
-	rightSidebarTemplate = template;
-	right_sidebar = new Ractive({
-						  el: 'right_sidebar',
-						  template: rightSidebarTemplate,
-						  data : {
-							signedIn: false,
-							user : {
-								pic: 'av.jpg',
-								name: 'Bubalus',
-								money: 900
-							}
-							}
-						  });
-});
- 
 
